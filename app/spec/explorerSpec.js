fda6b11a0f4beca63963bbcb06e95d75347398fe
Katie Smith <klssmith@users.noreply.github.com>
2016-01-13 10:22:50 +0000

    Adds spec files

---
+++ app/spec/explorerSpec.js
@@ -0,0 +1,23 @@
+describe("Explorer sprite", function() {
+  var explorer;
+
+  beforeEach(function() {
+    explorer = new Explorer();
+  });
+
+  describe("defaults", function() {
+    it("is stationary", function() {
+      expect(explorer.vx).toEqual(0);
+      expect(explorer.vy).toEqual(0);
+    });
+
+    it("is positioned at 0, 0", function() {
+      expect(explorer.x).toEqual(0);
+      expect(explorer.y).toEqual(0);
+    });
+
+    it("is not hit", function() {
+      expect(explorer.hit).toBe(false);
+    });
+  });
+});
