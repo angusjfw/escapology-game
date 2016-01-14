87653c4e2dacf3278635d623d18449bc6c68f1ba
hibreez <4ngus.whitehead@gmail.com>
2016-01-14 12:59:07 +0000

    Fixes issue with movement controls when changing levels with icy conditions undefined.

--- app/src/controls.js
+++ app/src/controls.js
@@ -6,52 +6,53 @@ function setUpControls(sprite) {
 
   left.press = function() {
     sprite.vx = -5;
-    if (!currentLevel().dungeon.isIcy) {
+    //adding check if defined. do for all. if undefined act as if not icy.
+    if (!currentLevel().dungeon || !currentLevel().dungeon.isIcy) {
       sprite.vy = 0;
     }
   };
   
   left.release = function() {
-    if (!currentLevel().dungeon.isIcy && !right.isDown && sprite.vy === 0) {
+    if ((!currentLevel().dungeon || !currentLevel().dungeon.isIcy) && !right.isDown && sprite.vy === 0) {
       sprite.vx = 0;
     }
   };
 
   up.press = function() {
     sprite.vy = -5;
-    if (!currentLevel().dungeon.isIcy) {
+    if (!currentLevel().dungeon || !currentLevel().dungeon.isIcy) {
       sprite.vx = 0;
     }
   };
 
   up.release = function() {
-    if (!currentLevel().dungeon.isIcy && !down.isDown && sprite.vx === 0) {
+    if ((!currentLevel().dungeon || !currentLevel().dungeon.isIcy) && !down.isDown && sprite.vx === 0) {
       sprite.vy = 0;
     }
   };
 
   right.press = function() {
     sprite.vx = 5;
-    if (!currentLevel().dungeon.isIcy) {
+    if (!currentLevel().dungeon || !currentLevel().dungeon.isIcy) {
       sprite.vy = 0;
     }
   };
 
   right.release = function() {
-    if (!currentLevel().dungeon.isIcy && !left.isDown && sprite.vy === 0) {
+    if ((!currentLevel().dungeon || !currentLevel().dungeon.isIcy) && !left.isDown && sprite.vy === 0) {
       sprite.vx = 0;
     }
   };
 
   down.press = function() {
     sprite.vy = 5;
-    if (!currentLevel().dungeon.isIcy) {
+    if (!currentLevel().dungeon || !currentLevel().dungeon.isIcy) {
       sprite.vx = 0;
     }
   };
 
   down.release = function() {
-    if (!currentLevel().dungeon.isIcy && !up.isDown && sprite.vx === 0) {
+    if ((!currentLevel().dungeon || !currentLevel().dungeon.isIcy) && !up.isDown && sprite.vx === 0) {
       sprite.vy = 0;
     }
   };
