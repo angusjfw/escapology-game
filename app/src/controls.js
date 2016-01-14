27c0b3ef15fce2ba8e5f58df1c7ae0b14e920756
hibreez <4ngus.whitehead@gmail.com>
2016-01-14 23:25:31 +0000

    Adds support for WASD and JHKL control keys.

--- app/src/controls.js
+++ app/src/controls.js
@@ -1,59 +1,66 @@
 function setUpControls(sprite) {
-  var left = keyboard(37),
-    up = keyboard(38),
-    right = keyboard(39),
-    down = keyboard(40);
+  var left = [keyboard(37), keyboard(65), keyboard(72)],
+    up = [keyboard(38), keyboard(87), keyboard(75)],
+    right = [keyboard(39), keyboard(68), keyboard(76)],
+    down = [keyboard(40), keyboard(83), keyboard(74)];
 
-  left.press = function() {
-    sprite.vx = -5;
-    //adding check if defined. do for all. if undefined act as if not icy.
-    if (!currentLevel().dungeon || !currentLevel().dungeon.isIcy) {
-      sprite.vy = 0;
-    }
-  };
-  
-  left.release = function() {
-    if ((!currentLevel().dungeon || !currentLevel().dungeon.isIcy) && !right.isDown && sprite.vy === 0) {
-      sprite.vx = 0;
-    }
-  };
+  left.forEach(function(leftKey) {
+    leftKey.press = function() {
+      sprite.vx = -5;
+      if (!currentLevel().dungeon || !currentLevel().dungeon.isIcy) {
+        sprite.vy = 0;
+      }
+    };
+    
+    leftKey.release = function() {
+      if ((!currentLevel().dungeon || !currentLevel().dungeon.isIcy) && !right.isDown && sprite.vy === 0) {
+        sprite.vx = 0;
+      }
+    };
+  });
 
-  up.press = function() {
-    sprite.vy = -5;
-    if (!currentLevel().dungeon || !currentLevel().dungeon.isIcy) {
-      sprite.vx = 0;
-    }
-  };
+  up.forEach(function(upKey) {
+    upKey.press = function() {
+      sprite.vy = -5;
+      if (!currentLevel().dungeon || !currentLevel().dungeon.isIcy) {
+        sprite.vx = 0;
+      }
+    };
 
-  up.release = function() {
-    if ((!currentLevel().dungeon || !currentLevel().dungeon.isIcy) && !down.isDown && sprite.vx === 0) {
-      sprite.vy = 0;
-    }
-  };
+    upKey.release = function() {
+      if ((!currentLevel().dungeon || !currentLevel().dungeon.isIcy) && !down.isDown && sprite.vx === 0) {
+        sprite.vy = 0;
+      }
+    };
+  });
 
-  right.press = function() {
-    sprite.vx = 5;
-    if (!currentLevel().dungeon || !currentLevel().dungeon.isIcy) {
-      sprite.vy = 0;
-    }
-  };
+  right.forEach(function(rightKey) {
+    rightKey.press = function() {
+      sprite.vx = 5;
+      if (!currentLevel().dungeon || !currentLevel().dungeon.isIcy) {
+        sprite.vy = 0;
+      }
+    };
 
-  right.release = function() {
-    if ((!currentLevel().dungeon || !currentLevel().dungeon.isIcy) && !left.isDown && sprite.vy === 0) {
-      sprite.vx = 0;
-    }
-  };
+    rightKey.release = function() {
+      if ((!currentLevel().dungeon || !currentLevel().dungeon.isIcy) && !left.isDown && sprite.vy === 0) {
+        sprite.vx = 0;
+      }
+    };
+  });
 
-  down.press = function() {
-    sprite.vy = 5;
-    if (!currentLevel().dungeon || !currentLevel().dungeon.isIcy) {
-      sprite.vx = 0;
-    }
-  };
+  down.forEach(function(downKey) {
+    downKey.press = function() {
+      sprite.vy = 5;
+      if (!currentLevel().dungeon || !currentLevel().dungeon.isIcy) {
+        sprite.vx = 0;
+      }
+    };
 
-  down.release = function() {
-    if ((!currentLevel().dungeon || !currentLevel().dungeon.isIcy) && !up.isDown && sprite.vx === 0) {
-      sprite.vy = 0;
-    }
-  };
+    downKey.release = function() {
+      if ((!currentLevel().dungeon || !currentLevel().dungeon.isIcy) && !up.isDown && sprite.vx === 0) {
+        sprite.vy = 0;
+      }
+    };
+  });
 }
